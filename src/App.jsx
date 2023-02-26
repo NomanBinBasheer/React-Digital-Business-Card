57a0f2d1f9a5e6db62340793325588f2cdf60809
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2023-02-26 12:05:02 +0500

    added all of the basic files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+import Info from './components/Info.jsx'
+
+import './App.css'
+
+function App() {
+  <Info />
+}
+
+export default App
---
+++ src/components/Info.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Info() {
+    return (
+        <section className="infoContainer">
+            <div className="imgContainer">
+                <img src="" alt="" />
+            </div>
+            <div className="headings">
+                <h1>Noman Bin Basheer</h1>
+                <h4>Front End Developer</h4>
+                <span><a>nomanbinbasheer.netlify.app</a></span>
+            </div>
+        </section>
+    )
+}
+
